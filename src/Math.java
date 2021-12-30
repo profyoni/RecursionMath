44a6cc9e041479d1caedc04b8750e9fcc767774b
Jonathan Robinson <Jonathan.Robinson@turkprime.com>
2021-12-30 15:33:45 -0500

    Initial commit

---
+++ src/Math.java
@@ -0,0 +1,19 @@
+public class Math {
+    public static int sum(int n, int m) {
+        return 0;
+    } //returns n + m via observation that adding m is same as adding 1 m times)
+
+    public static int product(int n, int m) {
+        return 0;
+    }//returns n * m via observation that multiplying n by m is same as adding n, m times)
+
+    public static int power(int n, int m) {
+        return 0;
+    }//returns n ^ m via observation that raising n to power m is same as multiplying n, m times)
+}
+
+class Solution {
+    public boolean isValid(String s) {
+        return false;
+    }
+}
